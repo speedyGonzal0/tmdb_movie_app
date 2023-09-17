4dd5d76cb968efa6ebf448afe8459558aa047836
Samin Ahsan <turzo68@gmail.com>
2023-09-18 02:07:20 +0600

    imeplement sidebar and complete layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import StyledComponentsRegistry from './registry'
+import localFont from 'next/font/local'
 import './globals.css'
+
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Sidebar from './components/sidebar/Sidebar'
+
+
+const materialSymbols = localFont({
+  variable: '--font-family-symbols', // Variable name (to reference after in CSS/styles)
+  style: 'normal',
+  src: '../node_modules/material-symbols/material-symbols-rounded.woff2', // This is a reference to woff2 file from NPM package "material-symbols"
+  display: 'block',
+  weight: '100 700',
+})
  
 export default function RootLayout({
   children,
@@ -7,9 +21,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html className={`${materialSymbols.variable}`}>
       <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Navbar/>
+        <Sidebar/>
+        <div className='h-screen bg-black pl-28'>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </div>
+        <Footer/>
       </body>
     </html>
   )
