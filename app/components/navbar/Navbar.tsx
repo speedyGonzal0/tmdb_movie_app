faa7e5dad0d772346b5babf92ed2877c43c79346
Samin Ahsan <turzo68@gmail.com>
2023-09-17 22:09:15 +0600

    refactor navbar elements

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styled from "styled-components";
-import Searchbutton from "./Searchbutton";
+import SearchButton from "./Searchbutton";
 
 const Logo = styled.h1`
   font-family: "Griffy", cursive;
@@ -20,19 +20,19 @@ type Link = {
 
 const navLinks: Link[] = [
   {
-    name: "HOME",
+    name: "Home",
     href: "/",
   },
   {
-    name: "TV SHOW",
+    name: "Tv Show",
     href: "/tvshows",
   },
   {
-    name: "MOVIES",
+    name: "Movies",
     href: "/movies",
   },
   {
-    name: "NEW",
+    name: "New",
     href: "/new",
   },
 ];
@@ -49,12 +49,12 @@ const Navbar = () => {
           {" "}
           <Logo> Dramatic </Logo>{" "}
         </div>
-        <div className="col-span-3 text-[#A1B1CB] font-mont flex items-center justify-evenly">
+        <ul className="col-span-3 text-[#A1B1CB] font-mont flex items-center justify-evenly uppercase">
           {navLinks.map((link) => {
             const isActive = pathName == link.href;
 
             return (
-              <div className="relative flex justify-center">
+              <li className="relative flex justify-center">
                 <Link
                   className={isActive ? activeClass : ""}
                   href={link.href}
@@ -62,19 +62,19 @@ const Navbar = () => {
                 >
                   {link.name}
                 </Link>
-                <div className={`absolute bg-[#38BBD8] h-2.5 w-2.5 top-6 rounded-xl ${isActive ? '' : 'hidden'}`}></div>
-              </div>
+                {isActive && <div className='absolute bg-[#38BBD8] h-2.5 w-2.5 top-6 rounded-xl'></div>}
+              </li>
             );
           })}
-        </div>
+        </ul>
         <div className="col-span-2 flex items-center justify-center">
-          <Searchbutton />
+          <SearchButton />
         </div>
-        <div className="col-span-2 flex items-center justify-evenly text-white">
-          <i className="symbol !text-3xl cursor-pointer"> redeem </i>
-          <i className="symbol !text-3xl cursor-pointer"> notifications </i>
-          <i className="symbol !text-3xl cursor-pointer"> account_circle </i>
-        </div>
+        <ul className="col-span-2 flex items-center justify-evenly text-white">
+          <li><i className="symbol !text-3xl cursor-pointer"> redeem </i></li>
+          <li><i className="symbol !text-3xl cursor-pointer"> notifications </i></li>
+          <li><i className="symbol !text-3xl cursor-pointer"> account_circle </i></li>
+        </ul>
       </div>
     </nav>
   );
--- app/components/navbar/Searchbutton.tsx
+++ app/components/navbar/Searchbutton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Searchbutton = () => {
+const SearchButton = () => {
   return (
     <button className="bg-[#3B567D] transition-all hover:bg-[#466186] w-4/5 flex justify-between text-[#A2B3CD] font-mont py-2 px-4 rounded-3xl">
       SEARCH
@@ -9,4 +9,4 @@ const Searchbutton = () => {
   );
 };
 
-export default Searchbutton;
+export default SearchButton;
